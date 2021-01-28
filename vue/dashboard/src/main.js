25408e8e23bb1fa0137e093bb62898142fffba77
pauloalchemist <paulolinsdev@gmail.com>
2021-01-28 20:05:00 -0300

    project init

---
+++ vue/dashboard/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App)
+  .use(router)
+  .mount('#app')
